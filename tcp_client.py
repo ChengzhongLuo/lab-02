8df54dd1c04b4cc442b1cfe8e3c010f9ec85ff2a
Chengzhong Luo <luoc@usc.edu>
2023-02-02 17:06:57 -0800

    change for tcp_client.py

--- tcp_client.py
+++ tcp_client.py
@@ -9,10 +9,46 @@ import socket
 
 def main():
     # TODO: Create a socket and connect it to the server at the designated IP and port
+
     # TODO: Get user input and send it to the server using your TCP socket
+
     # TODO: Receive a response from the server and close the TCP connection
+    # Create a socket object
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Get local machine name
+    host = socket.gethostname()
+
+    # Reserve a port for your service.
+    port = 12345
+
+    # Connect to the server
+    client_socket.connect((host, port))
+
+    # Send a short message
+    message = input("Enter message: ")
+    client_socket.sendall(message.encode('utf-8'))
+
+    # Receive data from the server
+    received_message = client_socket.recv(256).decode('utf-8')
+
+    # Print received message
+    print("Server says: " + received_message)
+
+    # Close the client socket
+    client_socket.close()
     pass
 
 
 if __name__ == '__main__':
     main()
+
+
+
+
+    import socket
+
+
+
+
+
